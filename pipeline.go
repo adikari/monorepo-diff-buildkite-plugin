62dfedbd239f567db66239b50549545f8a7ccef3
subash adhikari <me@subash.com.au>
2021-04-09 18:27:51 +1000

    refactor: move diff to pipeline file

--- pipeline.go
+++ pipeline.go
@@ -1,11 +1,32 @@
 package main
 
 import (
-	"fmt"
+	"strings"
+
+	log "github.com/sirupsen/logrus"
 )
+
+func diff(command string) []string {
+	log.Infof("Running diff command: %s", command)
+
+	split := strings.Split(command, " ")
+	cmd, args := split[0], split[1:]
+
+	output := executeCommand(cmd, args)
+
+	log.Debug("Output from diff:\n" + output)
+
+	return strings.Split(strings.TrimSpace(output), "\n")
+}
+
+func pipelinesToTrigger(diffCmd string) []string {
+	changedFiles := diff(diffCmd)
+
+	return changedFiles
+}
 
 func uploadPipeline() string {
 	msg := "uploading pipelines"
-	fmt.Println(msg)
+	log.Println(msg)
 	return msg
 }
--- pipeline_test.go
+++ pipeline_test.go
@@ -1,7 +1,31 @@
 package main
 
-import "testing"
+import (
+	"io/ioutil"
+	"os"
+	"testing"
 
+	log "github.com/sirupsen/logrus"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMain(m *testing.M) {
+	log.SetOutput(ioutil.Discard)
+	os.Exit(m.Run())
+}
+
+func TestDiff(t *testing.T) {
+	want := []string{
+		"services/foo/serverless.yml",
+		"services/bar/config.yml",
+		"ops/bar/config.yml",
+		"README.md",
+	}
+
+	got := diff("cat ./tests/mocks/diff1")
+
+	assert.Equal(t, want, got)
+}
 func TestUploadPipeline(t *testing.T) {
 	want := "uploading pipelines"
 	got := uploadPipeline()
