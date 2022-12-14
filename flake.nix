1bc6b8ea4e8360bf9db667879b28ceebe3dacd40
Jonathan Lorimer <jonathan_lorimer@mac.com>
2022-12-14 11:06:14 -0500

    feat: add nix for portability

---
+++ flake.nix
@@ -0,0 +1,47 @@
+{
+  description = "Monorepo plugin for buildkite";
+
+  inputs = {
+    # Nix Inputs
+    nixpkgs.url = github:nixos/nixpkgs/nixpkgs-unstable;
+    flake-utils.url = github:numtide/flake-utils;
+  };
+
+  outputs = {
+    flake-utils,
+    nixpkgs,
+    self
+  }:
+    with flake-utils.lib;
+      eachSystem
+      [
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ]
+      (system: let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+        version = "2.5.1";
+        monorepo-diff = pkgs.buildGo118Module ({
+          inherit version;
+          src = ./.;
+          vendorSha256 = pkgs.lib.fakeSha256;
+          pname = "monorepo-diff";
+          ldflags = ''
+            -X main.Version=${version}
+            -X main.BuildSha=${if (self ? rev) then self.rev else throw "You must commit git changes to run this binary"}
+          '';
+        });
+      in {
+        apps = {
+          default = mkApp {
+            name = "monorepo-diff";
+            drv = monorepo-diff;
+          };
+        };
+      });
+}
+
