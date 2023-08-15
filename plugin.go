f3233d352404da3952c08e88ecbcf8803ed26c94
Iaroslav Ciupin <iaroslav@union.ai>
2023-08-15 20:08:31 +0300

    Add support to pass build meta_data in trigger steps

--- plugin.go
+++ plugin.go
@@ -88,11 +88,12 @@ type Agent map[string]string
 
 // Build is buildkite build definition
 type Build struct {
-	Message string            `yaml:"message,omitempty"`
-	Branch  string            `yaml:"branch,omitempty"`
-	Commit  string            `yaml:"commit,omitempty"`
-	RawEnv  interface{}       `json:"env" yaml:",omitempty"`
-	Env     map[string]string `yaml:"env,omitempty"`
+	Message  string            `yaml:"message,omitempty"`
+	Branch   string            `yaml:"branch,omitempty"`
+	Commit   string            `yaml:"commit,omitempty"`
+	RawEnv   interface{}       `json:"env" yaml:",omitempty"`
+	Env      map[string]string `yaml:"env,omitempty"`
+	MetaData map[string]string `json:"meta_data,omitempty" yaml:"meta_data,omitempty"`
 	// Notify  []Notify          `yaml:"notify,omitempty"`
 }
 
